61cc6e0347aa1f8c837efe9ff7075a5d6f73303e
K <kqlykq@gmail.com>
2022-12-01 03:27:24 -0500

    a9 Mongo fa22 update

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,13 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  userName: String,
+  handle: String,
+  time: String,
+  image: String,
+  title: String,
+  topic: String,
+  replies: Number,
+  retuits: Number
 }, {collection: 'tuits'});
 export default schema;
