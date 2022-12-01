1fda08d6ed71e8876bafcc791edf767502345e37
K <kqlykq@gmail.com>
2022-12-01 03:21:40 -0500

    a9 Mongo fa22 update

--- app.js
+++ app.js
@@ -8,14 +8,13 @@ import TuitsController
   from "./controllers/tuits/tuits-controller.js";
 
 import mongoose from "mongoose";
-
+const CONNECTION_STRING = `${process.env.DB_CONNECTION_STRING}/test` || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
-const CONNECTION_STRING = `${process.env.DB_CONNECTION_STRING}/test` || 'mongodb://localhost:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
