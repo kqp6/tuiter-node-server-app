c2910e4fd67fe556baf7e4113a47d3c1da7275e8
K <kqlykq@gmail.com>
2022-12-01 23:39:57 -0500

    a9 Mongo fa22 update

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ import UserController
   from "./controllers/users/users-controller.js";
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
-
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = `${process.env.DB_CONNECTION_STRING}/test` 
-mongoose.connect(CONNECTION_STRING);
+// const CONNECTION_STRING = `${process.env.DB_CONNECTION_STRING}/test` 
+// mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb+srv://kqp3:Eric199815101688109@cluster0.hypzqfc.mongodb.net/?retryWrites=true&w=majority');
 
 const app = express();
 app.use(cors());
